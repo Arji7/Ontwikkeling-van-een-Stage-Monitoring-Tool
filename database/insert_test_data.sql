63baf77b26df295bf2a518e42a57aa4855c0c3ca
FxH24 <farouk.hamdaoui@student.ehb.be>
2026-06-10 10:54:04 +0200

    Update insert_test_data.sql

---
+++ database/insert_test_data.sql
@@ -0,0 +1,21 @@
+USE stage_monitor;
+
+-- Test gebruikers (wachtwoord voor allemaal: Test1!)
+INSERT INTO gebruiker (voornaam, achternaam, email, wachtwoord_hash, actief) VALUES
+  ('Farouk',    'Student',    'student@ehb.be',    '$2b$10$Kf/f8/TYJ63/z2cmDwR6Mu7u0GZM04GbVGsMG.lrc.WDQkBysEGRW', TRUE),
+  ('Achraf',   'Docent',     'docent@ehb.be',     '$2b$10$Kf/f8/TYJ63/z2cmDwR6Mu7u0GZM04GbVGsMG.lrc.WDQkBysEGRW', TRUE),
+  ('Ayoube',    'Commissie',  'commissie@ehb.be',  '$2b$10$Kf/f8/TYJ63/z2cmDwR6Mu7u0GZM04GbVGsMG.lrc.WDQkBysEGRW', TRUE);
+
+-- Rollen koppelen
+INSERT INTO gebruiker_rol (gebruiker_id, rol_id)
+SELECT g.id, r.id FROM gebruiker g, rol r
+WHERE g.email = 'student@ehb.be'    AND r.naam = 'student';
+
+INSERT INTO gebruiker_rol (gebruiker_id, rol_id)
+SELECT g.id, r.id FROM gebruiker g, rol r
+WHERE g.email = 'docent@ehb.be'     AND r.naam = 'docent';
+
+INSERT INTO gebruiker_rol (gebruiker_id, rol_id)
+SELECT g.id, r.id FROM gebruiker g, rol r
+WHERE g.email = 'commissie@ehb.be'  AND r.naam = 'commissielid';
+
